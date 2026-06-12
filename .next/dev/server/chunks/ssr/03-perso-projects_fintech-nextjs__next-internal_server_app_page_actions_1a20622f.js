c11a4b2d5d7bc5efddc71133dfa03242c124db18
roshumba <roshumba.llewellyn@gmail.com>
2026-06-12 18:37:36 -0400

    chore(.next): remove .next folder from git tracking

--- .next/dev/server/chunks/ssr/03-perso-projects_fintech-nextjs__next-internal_server_app_page_actions_1a20622f.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/03-perso-projects/fintech-nextjs/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=03-perso-projects_fintech-nextjs__next-internal_server_app_page_actions_1a20622f.js.map
